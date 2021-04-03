ee6d49c7a14cad0210e177e3520f98227e10ee1e
NEVITHAPRAKASINI MANICKAM <63694004+NEVITHAPRAKASINI@users.noreply.github.com>
2021-04-03 21:48:23 +0530

    Add files via upload

---
+++ age calculator/agecalculator.js
@@ -0,0 +1,5 @@
+var age = prompt("what is your age?");
+var days = age*365.25;
+var result = `${age} years is roughly ${age * 365.25} days`;
+// alert(age +"  years is roughly " + days + " days" );
+alert(result);
